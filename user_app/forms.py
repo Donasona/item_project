13b8f04fdbb8c349953e8078634ec5322e90640a
DONA PAUL <donapaul901@gmail.com>
2025-12-11 10:46:04 +0530

    cmt

--- user_app/forms.py
+++ user_app/forms.py
@@ -14,6 +14,6 @@ class RegisterForm(forms.ModelForm):
         model = User
         fields = ['username','email', 'password']
 
-class Loginform(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField()
+# class Loginform(forms.Form):
+#     username = forms.CharField(max_length=20)
+#     password = forms.CharField()
